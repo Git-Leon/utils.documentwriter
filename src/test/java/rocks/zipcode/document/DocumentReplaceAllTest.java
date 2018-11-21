b6a214080beb51e251d6a95fe24a5ed344dbc0ed
Leon <leon@Leons-MacBook-Pro.local>
2018-11-21 10:40:14 -0500

    update

--- src/test/java/rocks/zipcode/document/DocumentReplaceAllTest.java
+++ src/test/java/rocks/zipcode/document/DocumentReplaceAllTest.java
@@ -15,12 +15,33 @@ public class DocumentReplaceAllTest {
         // given
         String fileName = "file.txt";
         Document documentWriter = new Document(fileName);
-        String contentToBeWritten = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
-        String expected = contentToBeWritten.replaceAll("e", "_");
+        String contentToBeWritten = "Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit,\nsed do eiusmod tempor incididunt\nut labore et dolore\nmagna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris\nnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate\nvelit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat\nnon proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+        String valueToReplace = "e";
+        String replacementValue = "_";
+        String expected = contentToBeWritten.replaceAll(valueToReplace, replacementValue);
         documentWriter.write(contentToBeWritten);
 
         // when
-        documentWriter.replaceAll("e", "_");
+        documentWriter.replaceAll(valueToReplace, replacementValue);
+
+        // then
+        String actual = documentWriter.read();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testReplace_newLine_withEmptyString() throws IOException {
+        // given
+        String fileName = "file.txt";
+        Document documentWriter = new Document(fileName);
+        String contentToBeWritten = "Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit,\nsed do eiusmod tempor incididunt\nut labore et dolore\nmagna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris\nnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate\nvelit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat\nnon proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+        String valueToReplace = "\n";
+        String replacementValue = "";
+        String expected = contentToBeWritten.replaceAll(valueToReplace, replacementValue);
+        documentWriter.write(contentToBeWritten);
+
+        // when
+        documentWriter.replaceAll(valueToReplace, replacementValue);
 
         // then
         String actual = documentWriter.read();
